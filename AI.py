f151d2b7d8f0b13aa721c69e4216a3d8ed581e31
chincy11 <chincy-wm19@student.tarc.edu.my>
2022-04-14 21:39:28 +0800

    by cy

--- AI.py
+++ AI.py
@@ -11,54 +11,68 @@ resp = {
         "They call me {0}".format(name),
         "My name is the {0}".format(name)],
 
-    "m1":[
-        "Welcome to Product Module \n" + "            Any question for you? \n"
+    "m1": [
+        "Welcome to Product Module \n" + \
+        "            What kind of product you want to ask?\n" + \
+        "            1. Chair\n            2. Table\n            3. Accessories\n"
 
     ],
     "m2": [
-        "Welcome to Order Module \n" +\
+        "Welcome to Order Module \n" + \
         "            Any question for you? \n"
 
     ],
     "m3": [
-        "Welcome to Customer Service Module \n" +\
+        "Welcome to Customer Service Module \n" + \
         "            Any question for you? \n" + \
-        "         1. Return and refund online order \n" +\
-        "         2. Corporate S3GF \n" +\
+        "         1. Return and refund online order \n" + \
+        "         2. Corporate S3GF \n" + \
         "         3. Contact Us \n"
-
 
     ],
     "back": ["Welcome back to the S3GF Shop's Home Page, \n" \
-           "            I am the S3GF's Chatbot, Jason \n" \
-           "            Please choose one of the module task:\n" \
-           "            ~ Furniture Product Module -> Press 'm1'\n" \
-           "            ~ Take Order Module -> Press 'm2'\n" \
-           "            ~ Customer Service Module -> Press 'm3'\n"],
+             "            I am the S3GF's Chatbot, Jason \n" \
+             "            Please choose one of the module task:\n" \
+             "            ~ Furniture Product Module -> Press 'm1'\n" \
+             "            ~ Take Order Module -> Press 'm2'\n" \
+             "            ~ Customer Service Module -> Press 'm3'\n"],
     "default": ["This is a default message!"]
-    }
+}
 
-respM1 = {
-    "Product": [
-        "product",]
+product_list = {
+    "1": [
+        "This is all of our chair product,\n" +
+        "            can ask any of this product\n" +
+        "            1.TTRacing Maxx Gaming Chair\n" +
+        "            2.TTRacing Maxx Air Threads Fabric Gaming Chair\n" +
+        "            3.TTRacing Duo V3 Gaming Chair"],
+    "default": ["Sorry, i dont understand",
+                "please give related information"]
+}
+product1 = {
+    "1": [
+        "1.TTRacing Maxx Gaming Chair\n"
+    ]
 }
 respM2 = {
     "Order": [
-        "order",]
+        "order", ]
 }
 respM3 = {
-    "Help":[
+    "Help": [
         "help me",
     ]
 }
+
+
 def res(message):
     global isM1
     global isM2
     global isM3
 
     if isM1:
-        if message in respM1:
-            bot_message = random.choice(respM1[message])
+        if message in product_list:
+            bot_message = random.choice(product_list[message])
         elif message == "back":
             isM1 = False
             isM2 = False
@@ -67,7 +81,6 @@ def res(message):
         else:
             print(message)
             bot_message = random.choice(resp["default"])
-
     elif isM2:
         if message in respM2:
             bot_message = random.choice(respM2[message])
@@ -118,6 +131,7 @@ def res(message):
 
     return bot_message
 
+
 def real(xtext):
     if "name" in xtext:
         ytext = "what's your name?"
@@ -129,9 +143,9 @@ def real(xtext):
         ytext = xtext
     return ytext
 
+
 def send_message(message):
-    #print((message)) # if want block the ytext
+    # print((message)) # if want block the ytext
     print(message)
     response = res(message)
     return response
-
